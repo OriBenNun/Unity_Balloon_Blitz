d8b76738c066dc3850c9821f9a7a95951ffae9df
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-03-02 19:20:36 +0200

    Added tint change to the bar according to the current temperature

--- Assets/Scripts/TemperatureBar.cs
+++ Assets/Scripts/TemperatureBar.cs
@@ -1,18 +1,42 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TemperatureBar : MonoBehaviour
 {
     private Slider _slider;
+    
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float initialTemperature = 40f;
 
+    private Gradient _temperatureGradient;
+    
     private void Start()
     {
         _slider = GetComponent<Slider>();
+        _slider.value = initialTemperature;
+        
+        _temperatureGradient = new Gradient();
+
+        GradientColorKey[] colorKeys = new GradientColorKey[2];
+        colorKeys[0].color = Color.blue;
+        colorKeys[0].time = 0.0f;
+        colorKeys[1].color = Color.red;
+        colorKeys[1].time = 1.0f;
+
+        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
+        alphaKeys[0].alpha = 1.0f;
+        alphaKeys[0].time = 0.0f;
+        alphaKeys[1].alpha = 1.0f;
+        alphaKeys[1].time = 1.0f;
+
+        _temperatureGradient.SetKeys(colorKeys, alphaKeys);
     }
 
     public void ChangeCurrentTemperature(float valueToAdd)
     {
         _slider.value += valueToAdd;
+        fillImage.color = _temperatureGradient.Evaluate(_slider.normalizedValue);
     }
+
+    public void ResetTemperature() => _slider.value = initialTemperature;
 }
