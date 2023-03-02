ec03262d8909df7116f6feb5bc08456ee9a2ccf7
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-03-02 15:38:06 +0200

    Cleaned code

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class BalloonController : MonoBehaviour
 {
 
-    private Rigidbody2D rb2d;
-    private Transform transform;
+    private Rigidbody2D _rb2d;
+    private Transform _transform;
     
     [SerializeField] private float upFireForce = 0.3f;
     [SerializeField] private float turnForce = 0.3f;
@@ -16,43 +13,49 @@ public class BalloonController : MonoBehaviour
 
     private void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        transform = GetComponent<Transform>();
+        _rb2d = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
     }
 
     private void FixedUpdate()
     {
-        var xForce = 0f;
-        var yForce = 0f;
-        
-        if (Input.GetKey(KeyCode.Space) && transform.position.y <= disableFireForceYHeight)
-        {
-            yForce = upFireForce;
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            xForce = -turnForce;
-            
-        } 
-        else if (Input.GetKey(KeyCode.D))
-        {
-            xForce = turnForce;
-        }
-        
-        rb2d.AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+        ApplyMovementByInput();
+    }
 
-        if (transform.position.y <= deathYHeight)
+    private void Update()
+    {
+        if (_transform.position.y <= deathYHeight)
         {
             Death();
         }
     }
 
+    private void ApplyMovementByInput()
+    {
+        var xForce = 0f;
+        var yForce = 0f;
 
+        if (Input.GetKey(KeyCode.Space) && _transform.position.y <= disableFireForceYHeight)
+        {
+            yForce = upFireForce;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xForce = -turnForce;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xForce = turnForce;
+        }
+
+        _rb2d.AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+    }
+    
     private void Death()
     {
         print("DEAD!");
-        transform.position = new Vector3(0, 0, 0);
-        rb2d.velocity = new Vector2(0, 0);
+        _transform.position = new Vector3(0, 0, 0);
+        _rb2d.velocity = new Vector2(0, 0);
     }
 }
