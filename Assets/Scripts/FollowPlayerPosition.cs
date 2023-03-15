f1f67caf04f4bad8183e31dbf746c46c8c1775c2
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-03-15 16:05:46 +0200

    Added zindex to the FollowPlayerPosition.cs

--- Assets/Scripts/FollowPlayerPosition.cs
+++ Assets/Scripts/FollowPlayerPosition.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class FollowPlayerPosition : MonoBehaviour
 {
-    
+    [SerializeField] private int zPosition = 0;
     [SerializeField] private Transform playerTransform;
     
     private void Start()
@@ -15,7 +15,7 @@ public class FollowPlayerPosition : MonoBehaviour
     {
         while (true)
         {
-            transform.position = new Vector3(0, playerTransform.position.y, 0);
+            transform.position = new Vector3(0, playerTransform.position.y, zPosition);
             yield return new WaitForSeconds(cooldown);
         }
         // ReSharper disable once IteratorNeverReturns
